40cecf204bd582d460b53a21744cad2fe0d606a8
bezstrok <pocandre87@gmail.com>
2024-09-15 17:59:29 +0300

    Implement user logic

--- students/k33422/Malaev_Stepan/lr_1/src/endpoints/__init__.py
+++ students/k33422/Malaev_Stepan/lr_1/src/endpoints/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from .authorization import router as authorization_router
+from .user import router as user_router
 
 __all__ = ["router"]
 
 router = APIRouter()
 
 router.include_router(authorization_router)
+router.include_router(user_router)
--- students/k33422/Malaev_Stepan/lr_1/src/schemas/user.py
+++ students/k33422/Malaev_Stepan/lr_1/src/schemas/user.py
@@ -1,11 +1,20 @@
 import typing as tp
 
+from pydantic import EmailStr
+
 from . import base
 
-__all__ = ["UserGetDTO"]
+__all__ = [
+    "UserGetDTO",
+    "UserUpdateDTO",
+]
 
 
 class UserGetDTO(base.Base):
     id: int
     username: str
     email: tp.Optional[str]
+
+
+class UserUpdateDTO(base.Base):
+    email: EmailStr
